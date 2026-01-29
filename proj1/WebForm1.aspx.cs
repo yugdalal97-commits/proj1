9c0a9e3c54576b76c420b24cd972a02b8358cd05
EXTREMEDOPE <yugdalal97@gmail.com>
2026-01-29 19:20:55 +0530

    ddl done

--- proj1/WebForm1.aspx.cs
+++ proj1/WebForm1.aspx.cs
@@ -18,5 +18,50 @@ namespace proj1
         {
 
         }
+
+        protected void ddl_state_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddl_city.Items.Clear();
+            if (ddl_state.SelectedValue == "Maharashtra")
+            {
+                ddl_city.Items.Add("Mumbai");
+                ddl_city.Items.Add("Pune");
+                ddl_city.Items.Add("Nagpur");
+            }
+            else if (ddl_state.SelectedValue == "Gujarat")
+            {
+                ddl_city.Items.Add("Ahmedabad");
+                ddl_city.Items.Add("Surat");
+                ddl_city.Items.Add("Vadodara");
+            }
+            else if (ddl_state.SelectedValue == "Rajasthan")
+            {
+                ddl_city.Items.Add("Jaipur");
+                ddl_city.Items.Add("Udaipur");
+                ddl_city.Items.Add("Jodhpur");
+            }
+            else if (ddl_state.SelectedValue == "Tamil Nadu")
+            {
+                ddl_city.Items.Add("Chennai");
+                ddl_city.Items.Add("Coimbatore");
+                ddl_city.Items.Add("Madurai");
+            }
+            else if (ddl_state.SelectedValue == "Himachal Pradesh")
+            {
+                ddl_city.Items.Add("Shimla");
+                ddl_city.Items.Add("Manali");
+                ddl_city.Items.Add("Dharamshala");
+            }
+            else
+            {
+                ddl_city.Items.Add("Select City");
+                ddl_city.Items.Add("Bhai select kr ne");
+            }
+        }
+
+        protected void ddl_city_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
